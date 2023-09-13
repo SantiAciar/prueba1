a5cf53446830af57ad6635af49d567d593c9b25e
Santi Aciar <santiaciar04gmail.com>
2023-09-13 20:38:22 -0300

    prueba 1 de git

---
+++ programa1.php
@@ -0,0 +1,26 @@
+<?php
+  $dia = 24; //Se declara una variable de tipo integer.
+  $sueldo = 758.43; //Se declara una variable de tipo double.
+  $nombre = "juan"; //Se declara una variable de tipo string.
+  $exite = true; //Se declara una variable boolean.
+  echo "Variable entera:";
+  echo $dia;
+  echo "<br>";
+  echo "Variable double:";
+  echo $sueldo;
+  echo "<br>";
+  echo "Variable string:";
+  echo $nombre;
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $exite;
+  function SumarNumero($num1, $num2,$num3){
+
+    return $num1+$num2+$num3;
+    }
+    $Total=SumarNumero(10,20,30);
+    echo "El Total es : $Total"
+?>
+
+
+
